21e03af31cc8fe0567765ace068b3fc8b9405850
qo4rmp1 <qo4rmp1@gmail.com>
2017-08-09 16:43:14 +0800

    修正filter寫法

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,6 +19,6 @@ export class AppComponent {
   }
 
   doClertodos() {
-    this.todos = this.todos.filter(todo=>{!todo.done});
+    this.todos = this.todos.filter(todo=>{ return !todo.done; });
   }
 }
