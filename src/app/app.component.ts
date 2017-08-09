c5e3f76c4112d53975bc72680b266b5f775dd8ac
qo4rmp1 <qo4rmp1@gmail.com>
2017-08-09 16:23:45 +0800

    Task07

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  inputHint = 'What needs to be done?';
+  todos: any[] = [];
+  todo = '';
+
+  addtodo() {
+    this.todos.push(this.todo);
+    this.todo = '';
+  }
 }
